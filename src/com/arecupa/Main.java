a196c56af087b3ad9f056ae093ab5edf8e7963e8
Herick Guillen <herickguillen@yahoo.com>
2021-05-12 23:42:22 -0500

    My name

--- src/com/arecupa/Main.java
+++ src/com/arecupa/Main.java
@@ -2,11 +2,11 @@ package com.arecupa;
 
 public class Main {
     public void sayHello(String name){
-        System.out.println("Hello "+name+"!");
+        System.out.println("Hello my name is "+name+"!");
     }
 
     public static void main(String[] args){
         Main hello = new Main();
-        hello.sayHello("Bob");
+        hello.sayHello("Herick");
     }
 }
